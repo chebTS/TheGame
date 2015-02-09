71831547eec07ca31de2a8027d4631daa58c10fa
chebts <chebTS@gmail.com>
2015-02-10 01:06:43 +0200

    click

--- TheGame/app/src/main/java/ua/ck/android/thegame/MainActivity.java
+++ TheGame/app/src/main/java/ua/ck/android/thegame/MainActivity.java
@@ -6,19 +6,48 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
     private static final String TAG = MainActivity.class.getSimpleName();
+    private String buffer = "";
+    private TextView txtDebug;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtDebug = (TextView)findViewById(R.id.txt_debug);
         findViewById(R.id.left).setOnClickListener(this);
         findViewById(R.id.right).setOnClickListener(this);
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+                txtDebug.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(buffer.startsWith("LRRL")){
+                            Toast.makeText(MainActivity.this, "Atack", Toast.LENGTH_SHORT).show();
+                        }else if (buffer.startsWith("RLLR")){
+                            Toast.makeText(MainActivity.this, "Defence", Toast.LENGTH_SHORT).show();
+                        }else if (buffer.startsWith("RRRL")){
+                            Toast.makeText(MainActivity.this, "Heal", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(MainActivity.this, "No action", Toast.LENGTH_SHORT).show();
+                        }
+                        buffer = "";
+                        txtDebug.setText(buffer);
+                    }
+                });
+            }
+        }, 3000, 3000);
     }
 
 
@@ -27,9 +56,13 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         switch (v.getId()){
             case R.id.left:
                 Log.i(TAG, "Left");
+                buffer = buffer + "L";
+                txtDebug.setText(buffer);
                 break;
             case R.id.right:
                 Log.i(TAG, "Right");
+                buffer = buffer + "R";
+                txtDebug.setText(buffer);
                 break;
         }
     }
