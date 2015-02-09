0ce37da2e687e8baf245545ecdf10c949e66a097
chebts <chebTS@gmail.com>
2015-02-10 00:52:33 +0200

    bg

--- TheGame/app/src/main/java/ua/ck/android/thegame/MainActivity.java
+++ TheGame/app/src/main/java/ua/ck/android/thegame/MainActivity.java
@@ -2,38 +2,37 @@ package ua.ck.android.thegame;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.left).setOnClickListener(this);
+        findViewById(R.id.right).setOnClickListener(this);
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.left:
+                Log.i(TAG, "Left");
+                break;
+            case R.id.right:
+                Log.i(TAG, "Right");
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+
 }
