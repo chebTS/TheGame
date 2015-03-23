bff1da2987ecf93e3443ddf9b5b8fc9fda53a23e
chebts <chebTS@gmail.com>
2015-03-23 02:17:11 +0200

    menu added

---
+++ TheGame/app/src/main/java/ua/ck/android/thegame/MenuActivity.java
@@ -0,0 +1,30 @@
+package ua.ck.android.thegame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+
+
+
+/**
+     * Created by cheb on 3/23/15.
+ */
+public class MenuActivity extends ActionBarActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        findViewById(R.id.txt_play).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.txt_play:
+                    startActivity(new Intent(MenuActivity.this, MainActivity.class));
+                break;
+        }
+    }
+}
