ade4668ef0964cef0b0627dc47fb7be6554d7e8c
ChebTS <chebTS@gmail.com>
2015-04-14 00:47:12 +0300

    Menu

--- TheGame/app/src/main/java/ua/ck/android/thegame/LevelsActivity.java
+++ TheGame/app/src/main/java/ua/ck/android/thegame/LevelsActivity.java
@@ -4,31 +4,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
 
 /**
  * Created by cheb on 4/5/15.
  */
-public class LevelsActivity extends ActionBarActivity implements AdapterView.OnItemClickListener {
-    private ListView listView;
-    final String[] lavels = new String[] {
-            "Level 1", "Level 2", "Level 3"
-    };
+public class LevelsActivity extends ActionBarActivity implements View.OnClickListener {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_levels);
-        listView = (ListView)findViewById(R.id.list);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, lavels);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(this);
+        findViewById(R.id.btn_1).setOnClickListener(this);
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        startActivity(new Intent(LevelsActivity.this, MainActivity.class));
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_1:
+                startActivity(new Intent(LevelsActivity.this, MainActivity.class));
+                break;
+            case R.id.btn_2:
+                startActivity(new Intent(LevelsActivity.this, MainActivity.class));
+                break;
+            case R.id.btn_3:
+                break;
+
+        }
     }
+
+
 }
